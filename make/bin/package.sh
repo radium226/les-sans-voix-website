ed03ccd8349f3cf94b77cd6d3a700142a145ee44
radium226 <adrien.besnard@gmail.com>
2022-05-31 16:00:46 +0200

    Update package

--- make/bin/package.sh
+++ make/bin/package.sh
@@ -4,14 +4,19 @@ set -Eeuo pipefail
 
 main()
 {
-  mkdir -p "build/package/hugo"
+  mkdir -p "build/package"
 
+  # Building Hugo Theme
+
+  # Building Hugo
+  mkdir -p "build/package/hugo"
   hugo \
     --source "hugo" \
 		--cleanDestinationDir \
-		--destination "build/package/hugo" \
+		--destination "../build/package/hugo" \
 		--quiet
-    
+  
+  # Creating the archive
   tar -cvf "build/package/website.tar.gz" -C "build/package/hugo" "."
 }
 
