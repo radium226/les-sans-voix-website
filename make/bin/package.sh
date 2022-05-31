dd2e754d84a47b32a2df81680868b69bb59037be
radium226 <adrien.besnard@gmail.com>
2022-05-31 15:14:51 +0200

    Add constrast theme

--- make/bin/package.sh
+++ make/bin/package.sh
@@ -4,8 +4,13 @@ set -Eeuo pipefail
 
 main()
 {
-    mkdir -p "build/package"
-    tar -cvf "build/package/website.tar.gz" "index.html"
+    mkdir -p "build/package/hugo"
+    hugo  \
+		--cleanDestinationDir \
+		--destination "build/package/hugo" \
+		--quiet
+    
+    tar -cvf "build/package/website.tar.gz" "build/package/hugo"
 }
 
 main "${@}"
