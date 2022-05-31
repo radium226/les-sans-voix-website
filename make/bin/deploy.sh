eab0b416415627989a7c9ee8afa07fa238200206
radium226 <adrien.besnard@gmail.com>
2022-05-31 14:28:39 +0200

    Try auto-deploy with make

--- make/bin/deploy.sh
+++ make/bin/deploy.sh
@@ -17,11 +17,6 @@ main()
     git -C "${repo_folder_path}" config user.email "github-actions[bot]"
     git -C "${repo_folder_path}" config user.name "Robot"
 
-    #git config --global user.name "github-actions[bot]"
-    #git config --global user.email "41898282+github-actions[bot]@users.noreply.github.com"
-
-    https://your_username:$GITHUB_TOKEN@github.com/your/repo
-
     #git clean -fdx
     cp "./index.html" "${repo_folder_path}/index.html"
     git -C "${repo_folder_path}" add --all
